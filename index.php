223df158f5ff2f99e2b4be7eb94347863d8e64c4
Kesiena <52493905+Kesamg@users.noreply.github.com>
2023-07-24 13:09:57 +0200

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
     <meta name="description" content="See the latest NFL teams list. Get the best NFL football team coverage at ACME Sports.">
     <meta name="robots" content="index, follow">
 
-    <link rel="icon" type="image/x-icon" href="/acme/images/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="./images/favicon.ico">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdn.datatables.net/1.13.5/css/jquery.dataTables.css" />
     <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossorigin="anonymous"></script>
@@ -24,12 +24,12 @@
       })(jQuery);
 	  </script>
   </head>
-  <body style="background-image: url('/acme/images/nfl-main.png');background-size: 217px;background-color: #ecf2f7 !important;">
+  <body style="background-image: url('./images/nfl-main.png');background-size: 217px;background-color: #ecf2f7 !important;">
     <div class="container py-3" style="background: #fff;">
       <header>
         <div class="d-flex flex-column flex-md-row align-items-center pb-3 mb-4 border-bottom">
           <a href="/" class="d-flex align-items-center text-dark text-decoration-none">
-            <img src="/acme/images/logo.png" alt="ACME Sports Logo" width="186" height="112"/>
+            <img src="./images/logo.png" alt="ACME Sports Logo" width="186" height="112"/>
           </a>
     
         </div>
@@ -88,7 +88,7 @@
       <footer class="pt-4 my-md-5 pt-md-5 border-top">
         <div class="row">
           <div class="col-12 col-md">
-            <img class="mb-2" src="/acme/images/footer-logo.png" alt="ACME Sports Footer Icon" width="48" height="48">
+            <img class="mb-2" src="./images/footer-logo.png" alt="ACME Sports Footer Icon" width="48" height="48">
             <small class="d-block mb-3 text-muted">&copy; 2023 ACME Sports</small>
           </div>
         </div>
